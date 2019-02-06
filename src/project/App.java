9d13603b8b20198e1c3efc6cbc069c388f459ced
Daria <dashasemenyuta@gmail.com>
2019-02-07 01:25:35 +0200

    InsertionSort

--- src/project/App.java
+++ src/project/App.java
@@ -5,7 +5,7 @@ import project.view.CmdClientService;
 
 public class App {
 
-    public static int[] arr = {1, 3, -4 , 6, 0, 2};
+    public static final int[] arr = {1, 3, -4 , 6, 0, 2};
     public static void main(String[] args) {
         CmdClientService cmd = new CmdClientService();
         cmd.showMenuItems();
