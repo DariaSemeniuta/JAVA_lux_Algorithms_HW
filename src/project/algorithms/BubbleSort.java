a6345634bc7bc11ef2fbcc7a9b0bc1105b80c45c
Daria <dashasemenyuta@gmail.com>
2019-02-07 02:15:06 +0200

    bubble sort

---
+++ src/project/algorithms/BubbleSort.java
@@ -0,0 +1,17 @@
+package project.algorithms;
+
+public class BubbleSort implements Algorithms {
+    @Override
+    public void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = n - 1; j > i; --j) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
+    }
+}
